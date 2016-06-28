e9bdb81bdf3554b4a11e1542ab970c43d3132357
Manoroth Thosy <manoroththosy@gmail.com>
2016-06-28 00:25:11 -0700

    Add class and instances

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+PUPPY--------------------
+French Bull Dog 
+
+CHARACTERISTICS ---------
+Fur Color: varies
+Eye Color: varies
+Friendly: yes
+Size: compact
+Drooling: minimal
+
+
+
+BEHAVIOR ----------------
+Playful
+Energetic
+Intense
