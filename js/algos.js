c95a062d201aeef998d68c2212a53c7e79c1004c
Manoroth Thosy <manoroththosy@gmail.com>
2016-07-20 00:18:58 -0700

    Add pseudocode for release 0

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+//Release 0: Pusedocode 
+
+//create array with phrases
+
+//create function
+	//do something:
+		//find a way to manipulate the phrase to return the longest word or phrase
+//add drive code to test function
+
+	
