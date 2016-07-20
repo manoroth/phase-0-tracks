9d7a4aad6f794e2132f4b3a90681c45b6b40ebc7
Manoroth Thosy <manoroththosy@gmail.com>
2016-07-20 00:22:17 -0700

    Add function for release: 0

--- js/algos.js
+++ js/algos.js
@@ -7,4 +7,23 @@
 		//find a way to manipulate the phrase to return the longest word or phrase
 //add drive code to test function
 
+var pokemon_arr = ["pikachu", "bulbasaur", "squirtle", "charmander"];
+
+function find_longest_name(string_arr) {
 	
+	var index = 0;
+	var biggest_length = 0;
+	var longest_name = ""
+	
+	for(var i =0; i < string_arr.length; i++)
+	{
+		if(string_arr[i].length > biggest_length)
+		{
+			biggest_length = string_arr[i].length;
+			longest_name = string_arr[i];
+		}
+	}
+	return longest_name;
+}
+
+console.log(find_longest_name(pokemon_arr));
