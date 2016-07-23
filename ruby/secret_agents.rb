d776307748949bd3b4b1340b5f1d97426822cd70
Manoroth Thosy <manoroththosy@gmail.com>
2016-07-23 13:46:29 -0700

    files that I will not use

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,83 +1,58 @@
-def encrypt(string)
-#next line starts the body of the method 
-  index = 0 
-#use loop: while index = 0 is lesser than "abc" of the length   
-  while index < string.length 
-# letter = "abc" of the index, which gives it [0,1,2]  
-    letter =  string[index]
-    
-    if letter == "z"  
-      
-      string[index] = "a"
-      
-    elsif letter != " "  
-    
-      string[index] = string[index].next
-      
+#Once you're familiar with the sample code linked above, pseudocode (or write out the algorithms for, in other words) the following two methods in phase-0-tracks/ruby/secret_agents.rb. Then write the methods beneath your pseudocode. Do not use .each or .map. (We'll get to those next week.)
+
+#An encrypt method that advances every letter of a string one letter forward.
+#So "abc" would become "bcd". 
+#For now, you can assume lowercase input and output. 
+#Any space character should remain a space character -- no change made.
+
+def encrypt(words)
+   index = 0 
+   
+   while index < words.length 
+   
+    if words[index] == 'z'
+      words[index] = 'a'
+    elsif words[index] == ' '
+      words[index] = ' '
+    else 
+    words[index] = words[index].next 
     end 
-    
-    index += 1 
-    
-  end 
+      index += 1 
+   end 
+  #break words into index
+  #take index of alphabet then return next letter of alphabet 
+  #have space character equal a space 
+  puts words 
   
-  return string 
-#last line end tells us we're done defining the moethod
 end 
 
-def decrypt(string)
-  index = 0 
-  alpha_reverse = "zyxwvutsrqponmlkjihgfedcba"
-  
-  while index < string.length 
-    letter = string[index]
-    reversed_index = alpha_reverse.index(letter)
-    
-    if letter == "a"
-      string[index] = "z" 
-    elsif letter != " "
-      string[index] = alpha_reverse[reversed_index + 1]
-    end
+#encrypt("abc")
+#encrypt("zed")
+#A decrypt method that reverses the process above. 
+#Note that Ruby doesn't have a built-in method for going backward one letter
+#How can you find out where a letter is in the alphabet, then access the letter right before it? 
+#Hint: In the IRB session above, you learned how to ask a string for the index of a letter. "abcdefghijklmnopqrstuvwxyz" counts as a string.
 
-    index += 1 
+def decrypt(words)
+  #break the words into indexs again 
+  #take index of alphabet then returning the letter before the alphabet
+index = 0 
+alphabet = "abcdefghijklmnopqrstuvwxyz" 
+
+  while index < words.length 
+  alphabet.index(words[index]) - 1 
+  
+    until words[index] = alphabet[index]
+      alphabet[index] += 1 
+    end 
+#   print alphabet[location]
+# words[index] = alphabet[index] - 1 
+  index += 1 
   end 
-  return string 
+  words  
 end 
 
-# puts encrypt("abc")
-# puts encrypt("zed") 
-# puts decrypt("bcd") 
-# puts decrypt("afe")
 
-puts decrypt(encrypt("swordfish"))
-#you can say the decrypt is an object that is useing the encrypt'swordfish' as a variable.
-#something similar to the example below:
+puts decrypt("bcd")
 
 
-# def math(x)
-#   return (x + 1)
-# end
-# x = 1
-# 5.times do |i|
-#   x = math(x)
-# end
-# p x
-# # p math(x) == math(math(1))
-# # math(1) => 2
-# # math(2) => 3
-# p math(math(math(math(math(1)))))
-
-
-puts "Would like to encrypt or decrypt a password?"
-password = gets.chomp 
-
-if password == "encrypt"
-  puts "what is the password for encryption?"
-  user_password = gets.chomp
-  puts encrypt(user_password)
-elsif password == "decrypt"
-  puts "what is the password for decryption?"
-  user_password = gets.chomp
-  puts decrypt(user_password)
-else
-  puts "wrong"
-end
