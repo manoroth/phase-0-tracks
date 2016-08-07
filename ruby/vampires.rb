92fe984db243f30bbf4ff8914c924d7803d1f74b
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-06 21:53:34 -0700

    Add vampire data

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
