7781d1d64515c6f85fde9047614310e0aa98d60f
Manoroth Thosy <manoroththosy@gmail.com>
2016-08-09 17:21:02 -0700

    Add and tested methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -6,42 +6,42 @@
 
 
  "iNvEsTiGaTiOn".swapcase
-  => “InVeStIgAtIoN”
+  #=> “InVeStIgAtIoN”
   
 
  "zom".insert(2, "o")
-  => “zoom”
+  #=> “zoom”
   
  
  "enhance".center(15)
- => "    enhance    "
+ #=> "    enhance    "
   
 
  "Stop! You’re under arrest!".upcase
- => "STOP! YOU’RE UNDER ARREST!"
+ #=> "STOP! YOU’RE UNDER ARREST!"
   
 
  "the usual".ljust(18, " suspects")
-  => "the usual suspects"
+  #=> "the usual suspects"
   
  
  " suspects".rjust(18, 'the usual')
-  => "the usual suspects"
+  #=> "the usual suspects"
   
 
  "The case of the disappearing last letter".chop
-  => "The case of the disappearing last lette"
+  #=> "The case of the disappearing last lette"
   
  
- "The mystery of the missing first letter".[1, 39]
-  => "he mystery of the missing first letter"
+ "The mystery of the missing first letter".slice(1, 39)
+  #=> "he mystery of the missing first letter"
   
 
  "Elementary,    my   dear        Watson!".squeeze
-  => "Elementary, my dear Watson!"
+  #=> "Elementary, my dear Watson!"
 
  "z".ord
-  => 122 
+  #=> 122 
   # (What is the significance of the number 122 in relation to the character z?)
  # returns the codepoint of the first character of the string, assuming a single-byte character encoding
  #converting between characters and values or see a character code transfrom an ASCII code (number) into a string
